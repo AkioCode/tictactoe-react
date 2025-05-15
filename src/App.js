784badc124e99339cc8659d3f3e65c0dbf838156
Rodrigo Otsuka <akio2631@gmail.com>
2025-05-15 16:37:58 -0300

    Lift state to Board component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,76 @@
 import { useState } from 'react';
 
-var turn = 'X';
-
-function Square() {
-  const [value, setValue] = useState(null);
-  function handleClick() {
-    if (!value) {
-      setValue(turn);
+function updateWinnerSets(pos, winnerSets, currentPlayer, nextPlayer, remainingTurns){
+  const replaceAt = (arr) => arr.map((n) => n === pos ? currentPlayer : n); 
+  const hasBoth = (set) => [nextPlayer, pos].every((item) => set.includes(item))
+  return winnerSets.reduce((acc, set) => {
+    if (remainingTurns > 4 || !hasBoth(set)) {
+      return[...acc, replaceAt(set)];
+    } else {
+      return acc;
     }
-    turn = turn === 'X' ? 'O' : 'X';
-  }
-  return (<button className="square" onClick={handleClick}>{value}</button>);
+  }, []);
+}
+
+function Square({value, onSquareClick}) {
+  return (<button className="square" onClick={onSquareClick}>{value}</button>);
 }
 
 export default function Board() {
-  return <>
+  const initial_winner_sets = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  const [remainingTurns, setRemainingTurns] = useState(9);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [nextPlayer, setNextPlayer] = useState('O');
+  const [winnerSets, setWinnerSets] = useState(initial_winner_sets);
+  const [status, setStatus] = useState(`Next player: ${currentPlayer}`);
+  
+  function handleClick(i) {
+    if (!squares[i] && remainingTurns > 0) {
+      let nextSquares = squares.slice();
+      nextSquares[i] = currentPlayer;
+      setSquares(nextSquares);
+      
+      let nextWinnerSets = winnerSets.slice();
+      nextWinnerSets = updateWinnerSets(i, nextWinnerSets, currentPlayer, nextPlayer, remainingTurns);
+      setWinnerSets(nextWinnerSets);
+
+      let isWinner = (nextWinnerSets.some((set) => set.every((item) => item === currentPlayer)) ? currentPlayer : false);
+      setStatus(isWinner ? `Winner: ${currentPlayer}` : `Next player: ${currentPlayer}`);
+      setRemainingTurns(isWinner ? 0 : remainingTurns - 1);
+      if (!isWinner) {
+        setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+        setNextPlayer(nextPlayer === 'X' ? 'O' : 'X');
+      }
+    }
+  }
+    
+
+ return <>
+    <div className="status">{status}</div>
     <div className="board-row">
-      < Square />
-      < Square />
-      < Square />
+      < Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+      < Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+      < Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
     </div>
     <div className="board-row">
-      < Square />
-      < Square />
-      < Square />
+      < Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+      < Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+      < Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
     </div>
     <div className="board-row">
-      < Square />
-      < Square />
-      < Square />
+      < Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+      < Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+      < Square value={squares[9]} onSquareClick={() => handleClick(9)}/>
     </div>
   </>
 }
